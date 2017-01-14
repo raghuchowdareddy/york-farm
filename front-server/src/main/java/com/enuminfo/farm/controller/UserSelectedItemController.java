f07b27cd04c195d57572655511c78714009160dd
Kumar <Kumar@192.168.1.103>
2017-01-14 10:58:43 +0530

    UserSelectedProductItems from draft saving we need to take it as List not Array.

--- front-server/src/main/java/com/enuminfo/farm/controller/UserSelectedItemController.java
+++ front-server/src/main/java/com/enuminfo/farm/controller/UserSelectedItemController.java
@@ -1,12 +1,13 @@
 package com.enuminfo.farm.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.enuminfo.farm.model.UserSelectedItem;
@@ -16,10 +17,10 @@ import com.enuminfo.farm.service.IUserSelectedItem;
 public class UserSelectedItemController {
 
 	@RequestMapping(value="/draftSelectedItems",method = RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE)
-	public void draftSelectedItem(@RequestBody UserSelectedItem[] userSelectedItem){
-		for (UserSelectedItem userSelectedItem2 : userSelectedItem) {
-			System.out.println("UserSelectedItemController.draftSelectedItem():: "+userSelectedItem2.toString());
-			selectedItem.add(userSelectedItem2);
+	public void draftSelectedItem(@RequestBody List<UserSelectedItem> userSelectedItems){
+		for (UserSelectedItem userSelectedItem : userSelectedItems) {
+			System.out.println("UserSelectedItemController.draftSelectedItem():: "+userSelectedItem.toString());
+			selectedItem.add(userSelectedItem);
 		}
 		
 	}
@@ -29,5 +30,6 @@ public class UserSelectedItemController {
 		  return selectedItem.getDraftedItems(Integer.valueOf(userNumber));
 			
 	}
+	
 	@Autowired IUserSelectedItem selectedItem;
 }
